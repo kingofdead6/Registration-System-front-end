416387862b3a89063fcd16488e048dec6bfcf35c
Young Wolf <162513105+kingofdead6@users.noreply.github.com>
2024-12-09 23:56:58 +0100

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,13 @@
 <script setup>
+import Create_team from './Views/Create_team.vue';
+import Create_team2 from './Views/Create_team2.vue';
+import Join_team from './Views/Join_team.vue';
+import SignIn from './Views/SignIn.vue';
+import SignUp from './Views/SignUp.vue';
+import Staff_Format from './Views/Staff_Format.vue';
+
 </script>
 
 <template>
- <h1>Hello World</h1>
+<Create_team />
 </template>
