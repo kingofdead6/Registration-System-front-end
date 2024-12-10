6a70f76b2bc8063511e71c6a5f0f5038842542f6
yasminezegaoui <yasminezegaoui1@gmail.com>
2024-12-10 21:09:24 +0100

    NavBar file fixed

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,42 +1,31 @@
 <script setup>
 import logo from '/src/assets/img/gdg_logo.png'
-import {RouterLink} from 'vue-router'
+import {RouterLink} from 'vue-router';
+
+const navItems = [
+  { label: "GDG Events", icon: logo, isImage: true},
+  { label: "Home", link: "/" },
+  { label: "Sign in", link: "/sign_in" },
+  { label: "Sign up", link: "/sign_up" },
+];
 </script>
 
 <template>
-    <nav class="bg-[#F2EDED] border-none border-[#F2EDED]">
-      <div class="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
-        <div class="flex h-20 items-center justify-between">
-          <div
-            class="flex flex-1 items-center justify-center md:items-stretch md:justify-start"
-          >
-            <RouterLink class="flex items-center mr-4" to="/">
-              <img class="h-20 w-46" :src="logo" alt="GDG Logo" />
-              <span class="hidden md:block text-[#2B4DD4] text-2xl font-bold ml-2"
-                >GDG Events</span
-              >
-            </RouterLink>
-            <div class="md:ml-auto">
-              <div class="flex space-x-2">
-                <RouterLink
-                  to="/"
-                  class="text-[#2B4DD4] hover:bg-gray-300 hover:text-[#FF0000] text-lg rounded-md px-2 py-2 font-bold mt-5"
-                  >Home</RouterLink
-                >
-                <RouterLink
-                  to="/sign_in"
-                  class="text-[#2B4DD4] hover:bg-gray-300 hover:text-[#FF0000] text-lg rounded-md px-2 py-2 font-bold mt-5"
-                  >Sign in</RouterLink
-                >
-                <RouterLink
-                  to="/sign_up"
-                  class="text-[#2B4DD4] hover:bg-gray-300 hover:text-[#FF0000] text-lg rounded-md px-2 py-2 font-bold mt-5"
-                  >Sign up</RouterLink
-                >
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </nav>
+  <nav class="top-0 flex items-center justify-between bg-[#ebe1e1] px-4">
+
+    <div class="flex items-center space-x-2">
+      <img :src="navItems[0].icon" alt="GDG Logo" class="h-20 w-30 ml-[41px]" />
+      <RouterLink to="/" class="hidden md:block text-[#2B4DD4] text-2xl font-bold ml-2 font-inkut cursor-pointer">
+        {{ navItems[0].label }}
+      </RouterLink>
+    </div>
+
+    <ul class="flex items-center space-x-2 mr-[25px]">
+      <li v-for="(item, index) in navItems.slice(1)" :key="index" class="flex items-center ">
+        <RouterLink :to="item.link" class="text-[#2B4DD4] hover:bg-gray-300 hover:text-[#FF0000] text-lg rounded-md px-2 py-2 font-bold font-inkut">
+          {{ item.label }}
+        </RouterLink>
+      </li>
+    </ul>
+  </nav>
 </template>
