3904c3db7276c6a3e558f6e2990f9e26754a3794
Young Wolf <162513105+kingofdead6@users.noreply.github.com>
2024-12-11 21:30:45 +0100

    Update Registration_member_form.vue

--- src/views/Registration_member_form.vue
+++ src/views/Registration_member_form.vue
@@ -1,7 +1,6 @@
 <script setup>
 import { ref } from 'vue';
 import { useRouter } from 'vue-router';
-import NavBar from '../components/NavBar.vue'; 
 
 const router = useRouter();
 const teamID = ref('');
