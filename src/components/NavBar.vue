e51557ed37f7b054b3631b4fa77fad0ab8308be1
yasminezegaoui <yasminezegaoui1@gmail.com>
2024-12-11 19:26:54 +0100

    navbar fixed

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -4,9 +4,6 @@ import {RouterLink} from 'vue-router';
 
 const navItems = [
   { label: "GDG Events", icon: logo, isImage: true},
-  { label: "Home", link: "/" },
-  { label: "Sign in", link: "/sign_in" },
-  { label: "Sign up", link: "/sign_up" },
 ];
 </script>
 
@@ -19,13 +16,5 @@ const navItems = [
         {{ navItems[0].label }}
       </RouterLink>
     </div>
-
-    <ul class="flex items-center space-x-2 mr-[25px]">
-      <li v-for="(item, index) in navItems.slice(1)" :key="index" class="flex items-center ">
-        <RouterLink :to="item.link" class="text-[#2B4DD4] hover:bg-gray-300 hover:text-[#FF0000] text-lg rounded-md px-2 py-2 font-bold font-inkut">
-          {{ item.label }}
-        </RouterLink>
-      </li>
-    </ul>
   </nav>
 </template>
