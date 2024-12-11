1d7c433e4c6d383acfa8061b63122126f90d239d
yasminezegaoui <yasminezegaoui1@gmail.com>
2024-12-11 19:06:55 +0100

    event's detailed page added

--- src/main.js
+++ src/main.js
@@ -1,5 +1,9 @@
 import { createApp } from 'vue'
 import './assets/main.css';
+import 'primeicons/primeicons.css';
+import router from './router';
 import App from './App.vue'
 
-createApp(App).mount('#app')
+const app = createApp(App);
+app.use(router)
+app.mount('#app')
