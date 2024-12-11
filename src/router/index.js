70a01f01743a34cf30c68450838438859f44e8c8
Young Wolf <162513105+kingofdead6@users.noreply.github.com>
2024-12-11 20:52:42 +0100

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,13 @@ import { createRouter, createWebHistory } from "vue-router";
 import DashboardView from '../views/DashboardView.vue'
 import RegisterView from '../views/RegisterView.vue'
 import HomeView from '../views/HomeView.vue';
-
+import RegistrationForm from "../Views/RegistrationForm.vue";
+import RegistrationForm2 from "../Views/RegistrationForm2.vue";
+import Registration_Job_choice from "../Views/Registration_Job_choice.vue";
+import Registration_leader_form from "../Views/Registration_leader_form.vue";
+import Registration_member_form from "../Views/Registration_member_form.vue";
+import TeamCreated from "../Views/TeamCreated.vue";
+import TeamJoined from "../Views/TeamJoined.vue";
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
     routes: [
@@ -21,7 +27,49 @@ const router = createRouter({
             name: 'Home',
             component: HomeView
         },
+        {
+            path: '/registration_form',
+            name: 'registration_form',
+            component: RegistrationForm
+          },
+          {
+            path: '/registration_form2',
+            name: 'registration_form2',
+            component: RegistrationForm2
+          },
+          {
+              path :'/registration_Job_choice',
+              name : 'registration_Job_choice',
+              component : Registration_Job_choice
+          },
+          {
+              path : '/registration_leader_form',
+              name : 'registration_leader_form',
+              component : Registration_leader_form
+          },
+          {
+              path : '/registration_member_form' ,
+              name : 'registration_member_form',
+              component : Registration_member_form
+          },
+          {
+              path : '/teamcreated',
+              name : 'teamcreated',
+              component : TeamCreated
+          },
+          {
+              path : '/teamjoined' ,
+              name : 'teamjoined',
+              component : TeamJoined
+          }
     ]
 });
 
 export default router;
+
+
+   
+
+
+
+   
